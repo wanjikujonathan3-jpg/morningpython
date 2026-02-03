e2b4481c4c0868d3987b83488e8060b25bd117a0
wanjikujonathan3 <wanjikujonathan3@gmail.com>
2026-02-03 10:31:55 +0300

    coming soon

---
+++ introduction.py
@@ -0,0 +1,7 @@
+from itertools import product
+
+print("welcome to python programming")
+print("jonathan")
+print(100)
+print(20*3)
+#print is amethod used to display an output
---
+++ variables.py
@@ -0,0 +1,14 @@
+#variable is a container used to store a value
+school= "eMobilis"
+firstname = "Glory"
+lastname = "emobilis"
+num1 = 36
+num2 = 42
+language ="python"
+print(school)
+print(firstname,lastname)
+print(num1 + num2)
+
+print(language,"python is a web programming language")
+
+print("the sum of", num1 , "and", num2 , "is" ,num1+num2 )
