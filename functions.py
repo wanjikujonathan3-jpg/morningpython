e562e842fb076c9629fc9c0cdfbc67d107126e05
icey-netizen <viny4457@gmail.com>
2026-02-06 10:38:28 +0300

    add

---
+++ functions.py
@@ -0,0 +1,41 @@
+#Functions/Methods a block of code used to perform a task
+#Standard-library functions
+
+y = max (45 ,78, 454 ,46 ,565, 5657, 6767)
+print("The maximum number is",y)
+x=min(455,557,5676,576,756,47575,55858,)
+print("The minimum number is",x)
+
+# User-Defined functions
+def name():
+    print("jonathan")
+
+name() #calling a function
+
+
+def add():
+    print (10 + 20)
+add()
+
+#PARAMETER/vARIABLE
+#Arguments/Values
+def dog(name, breed ,age):
+    print(name,breed,age)
+
+dog("bob","german shepherd","8")
+dog("peter","siberian husky", "8")
+
+
+#A program to display details of fiveemployees at eMobilis using parameters and arguments
+
+def employee(fullname,position,gender,age):
+    print(fullname,position,gender,age)
+employee("sylvester manyanga","chairman","male","40")
+employee("emmanuel olor","secretary","male","28")
+employee("James bond","lecturer","male","50")
+employee("simon james","technician","male","34")
+employee("mathew vor","technician","male","40")
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,25 @@
+#while loop
+from itertools import count
+
+count=10
+while count <=15:
+    print(count)
+    count+=1
+print()
+
+#program
+number =105
+while number <=100:
+    print(number)
+    number+=1
+
+    #for loop
+for num in range( 20,26, 2  ):
+    print(num)
+
+
+languages=["Python","C++","Java"]
+for lang in languages:
+    print(lang)
+
+    #Break and continue statement
